954b862e1f16c4e25b5f55ddb98f1a62b8a48366
Sabrina <shasan06@dcs.bbk.ac.uk>
2021-03-23 14:12:14 +0000

    testing methods

--- src/piazza/tests.py
+++ src/piazza/tests.py
@@ -1,3 +1,77 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+from rest_framework.test import APIClient
+
+from .models import Tweet
 # Create your tests here.
+User = get_user_model()
+
+class TweetTestCase(TestCase):#Inherit test case
+    def setUp(self):
+        #User.objects.create_user(username='abc', password='somepassword')
+        self.user = User.objects.create_user(username='cfe', password='somepassword')
+        Tweet.objects.create(message="my first tweet", user=self.user)
+        Tweet.objects.create(message="my first tweet", user=self.user)
+        Tweet.objects.create(message="my first tweet", user=self.user)
+
+
+    def test_tweet_created(self):
+        tweet_obj = Tweet.objects.create(message="my second tweet", user=self.user)
+        self.assertEqual(tweet_obj.id, 4)#id of 4 because 3 are created up and the 4th one here
+        self.assertEqual(tweet_obj.user, self.user)
+    '''def test_user_created(self):
+        #user = User.objects.get(username="cfe")
+        self.assertEqual(self.user.username, "cfe")
+        self.assertEqual(1, 2)'''
+
+        #test the urls django rest framework testing -- find in api guide for testing
+
+
+    def get_client(self):
+        client = APIClient()
+        client.login(username=self.user.username, password='somepassword')
+        return client
+
+    def test_tweet_list(self):
+        client = self.get_client()
+        response = client.get("/api/piazza/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 1)
+        #print(response.json())
+
+    def test_tweet_list(self):
+        client = self.get_client()
+        response = client.get("/api/piazza/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 3)
+
+    def test_action_like(self):
+        client = self.get_client()
+        response = client.post("/api/piazza/action/", 
+            {"id": 1, "action": "likes"})
+        self.assertEqual(response.status_code, 200)
+        like_count = response.json().get("likes")
+        self.assertEqual(like_count, 1)
+
+    #for dislike
+    def test_action_dislike(self):
+        client = self.get_client()
+        response = client.post("/api/piazza/action/", 
+            {"id": 2, "action": "likes"})
+        self.assertEqual(response.status_code, 200)
+        response = client.post("/api/piazza/action/", 
+            {"id": 2, "action": "dislikes"})
+        self.assertEqual(response.status_code, 200)
+        like_count = response.json().get("likes")
+        self.assertEqual(like_count, 0)
+        #print(response.json())
+        #self.assertEqual(len(response.json()), 3)
+        #got an error when testing unlike eg None!=0 means app is not sending back the data
+    
+    #for comments/retweeting
+    def test_action_comment(self):
+        client = self.get_client()
+        response = client.post("/api/piazza/action/", 
+            {"id": 2, "action": "comments"})
+        self.assertEqual(response.status_code, 201)
--- src/piazza/views.py
+++ src/piazza/views.py
@@ -89,6 +89,8 @@ def tweet_action_view(request, *args, **kwargs):
             return Response(serializer.data, status=200)
         elif action == "dislikes":#unlike
             obj.likes.remove(request.user)
+            serializer = TweetSerializer(obj)
+            return Response(serializer.data, status=200)
         elif action == "comments":#retweet
             new_tweet = Tweet.objects.create(
                     user=request.user, 
@@ -96,7 +98,7 @@ def tweet_action_view(request, *args, **kwargs):
                     message=message,
                     )
             serializer = TweetSerializer(new_tweet)
-            return Response(serializer.data, status=200)
+            return Response(serializer.data, status=201)
     return Response({}, status=200)
 
 '''def tweet_like_toggle_view(request, tweet_id,  *args, **kwargs):
